0a0339be20cd86ca013eaf40b4d0e0cee6dd72fa
Derek <256792747+decofe@users.noreply.github.com>
2026-05-22 16:53:11 +0000

    fix react native provider test storage

--- src/react-native/Provider.ts
+++ src/react-native/Provider.ts
@@ -1,4 +1,5 @@
 import * as CoreProvider from '../core/Provider.js'
+import * as Storage from '../core/Storage.js'
 import { reactNative } from './adapter.js'
 import { asyncStorage } from './storage.js'
 
@@ -7,7 +8,7 @@ export function create(options: create.Options): create.ReturnType {
   const { host = 'https://wallet.tempo.xyz', redirectUri, open, secureStorage, ...rest } = options
 
   return CoreProvider.create({
-    storage: asyncStorage(),
+    storage: defaultStorage(),
     ...rest,
     adapter: reactNative({
       host,
@@ -16,6 +17,11 @@ export function create(options: create.Options): create.ReturnType {
       ...(secureStorage ? { secureStorage } : {}),
     }),
   })
+}
+
+function defaultStorage(): Storage.Storage {
+  if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') return asyncStorage()
+  return Storage.memory()
 }
 
 export declare namespace create {
